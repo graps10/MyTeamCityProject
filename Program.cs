c7031e4376e341cfad9e72facb152e288d204de7
Chpion <golobokov66@gmail.com>
2025-10-04 17:21:10 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyTeamCityProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, TeamCity!");
+        }
+    }
+}
